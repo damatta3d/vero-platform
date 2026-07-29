67ed4f05721746c47cb06fe37f06ba44b80be30d
damatta3d <christian.matta.ms@gmail.com>
2026-07-29 16:25:44 -0400

    feat(intelligence): publish fact contract

---
+++ packages/business/intelligence/src/public-api.ts
@@ -0,0 +1,12 @@
+export {
+  createChannelOrderFact,
+  InvalidChannelOrderFactError,
+  type ChannelOrderAdjustmentFact,
+  type ChannelOrderAdjustmentKind,
+  type ChannelOrderFact,
+  type ChannelOrderLineFact,
+  type ChannelOrderLineKind,
+  type CreateChannelOrderFactInput,
+  type OrderPseudonymizer,
+  type OrderPseudonymScope
+} from './domain/channel-order-fact.js';
